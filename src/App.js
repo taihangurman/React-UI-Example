ccf26bb05529fadbf97d9b0827c8a6e128895cba
Taihan Gurman <taihangurman56219@gmail.com>
2021-07-23 06:58:28 +0500

    publish

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Ui from "./Ui/index.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Ui />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Ui/index.js
@@ -0,0 +1,383 @@
+import React from "react";
+import "./Ui.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="zx-topHead">
+        <h2>text</h2>
+        <h2>text</h2>
+      </div>
+
+      <div className="zx-topHead2">
+        <h2>text</h2>
+        <h2 className="zx-topHead2heading">text</h2>
+      </div>
+
+      <div className="zx-topHead3">
+        <div className="zx-head3-box2">
+          <h2>text</h2>
+          <div className="zx-head3-mini-box">
+            <div className="zx-head3-mini1"></div>
+            <div className="zx-head3-mini2"></div>
+            <div className="head3-main">
+              <div className="zx-head3-mini3">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini4">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+            </div>
+            <div className="zx-head3-mini6">
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button style={{ marginLeft: "20px" }}>
+                <a href="">button</a>
+              </button>
+            </div>
+          </div>
+
+          <div className="zx-head3-mini-box">
+            <div className="zx-head3-mini1"></div>
+            <div className="zx-head3-mini2"></div>
+            <div className="head3-main">
+              <div className="zx-head3-mini3">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini4">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+            </div>
+            <div className="zx-head3-mini6">
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button style={{ marginLeft: "20px" }}>
+                <a href="">button</a>
+              </button>
+            </div>
+          </div>
+
+          <div className="zx-head3-mini-box">
+            <div className="zx-head3-mini1"></div>
+            <div className="zx-head3-mini2"></div>
+            <div className="head3-main">
+              <div className="zx-head3-mini3">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini4">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+            </div>
+            <div className="zx-head3-mini6">
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button style={{ marginLeft: "20px" }}>
+                <a href="">button</a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="zx-topHead3" style={{ marginTop: "40px" }}>
+        <div className="zx-head3-box2">
+          <div className="last-head">
+            <div>
+              <h2>text</h2>
+            </div>
+            <div className="last-head-box2">
+              <div style={{ margin: '20px' }}>
+                <small>text</small>
+                <button>
+                  <a href="">button</a>
+                </button>
+                <button>
+                  <a href="">button</a>
+                </button>
+                <button>
+                  <a href="">button</a>
+                </button>
+              </div>
+              <div>
+                <small>text</small>
+                <input type="text" />
+              </div>
+            </div>
+          </div>
+          <div className="zx-head3-mini-box">
+            <div className="zx-head3-mini1"></div>
+            <div className="zx-head3-mini2"></div>
+            <div className="head3-main">
+              <div className="zx-head3-mini3">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini4">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+            </div>
+            <div className="zx-head3-mini6">
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button style={{ marginLeft: "20px" }}>
+                <a href="">button</a>
+              </button>
+            </div>
+          </div>
+
+          <div className="zx-head3-mini-box">
+            <div className="zx-head3-mini1"></div>
+            <div className="zx-head3-mini2"></div>
+            <div className="head3-main">
+              <div className="zx-head3-mini3">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini4">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+            </div>
+            <div className="zx-head3-mini6">
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button style={{ marginLeft: "20px" }}>
+                <a href="">button</a>
+              </button>
+            </div>
+          </div>
+
+          <div className="zx-head3-mini-box">
+            <div className="zx-head3-mini1"></div>
+            <div className="zx-head3-mini2"></div>
+            <div className="head3-main">
+              <div className="zx-head3-mini3">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini4">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+            </div>
+            <div className="zx-head3-mini6">
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button style={{ marginLeft: "20px" }}>
+                <a href="">button</a>
+              </button>
+            </div>
+          </div>
+          <div className="zx-head3-mini-box">
+            <div className="zx-head3-mini1"></div>
+            <div className="zx-head3-mini2"></div>
+            <div className="head3-main">
+              <div className="zx-head3-mini3">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini4">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+            </div>
+            <div className="zx-head3-mini6">
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button style={{ marginLeft: "20px" }}>
+                <a href="">button</a>
+              </button>
+            </div>
+          </div>
+          <div className="zx-head3-mini-box">
+            <div className="zx-head3-mini1"></div>
+            <div className="zx-head3-mini2"></div>
+            <div className="head3-main">
+              <div className="zx-head3-mini3">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini4">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+            </div>
+            <div className="zx-head3-mini6">
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button style={{ marginLeft: "20px" }}>
+                <a href="">button</a>
+              </button>
+            </div>
+          </div>
+          <div className="zx-head3-mini-box">
+            <div className="zx-head3-mini1"></div>
+            <div className="zx-head3-mini2"></div>
+            <div className="head3-main">
+              <div className="zx-head3-mini3">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini4">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+              <div className="zx-head3-mini5">
+                <p>text</p>
+                <h3>text</h3>
+              </div>
+            </div>
+            <div className="zx-head3-mini6">
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button>
+                <a href="">button</a>
+              </button>
+              <button style={{ marginLeft: "20px" }}>
+                <a href="">button</a>
+              </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
